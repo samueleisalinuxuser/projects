681c3aa8c4e3bd5eca8dac66f46db1d4d0295ab8
Samuele <samuele.cistaro@gmail.com>
2024-03-17 12:53:33 +0100

    rewrote main.c

--- irc-client/src/main.c
+++ irc-client/src/main.c
@@ -5,88 +5,33 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#define EXPECTED_ARGC 4
 #define MAX_NICKNAME_LENGHT 9
 #define MAX_PASSWORD_LENGHT 20
-#define MAX_MESSAGES 2000
 
-int clientFd;
+char nickname[MAX_NICKNAME_LENGHT], password[MAX_PASSWORD_LENGHT], rawIRCServers[500];
 
-struct hostent *LiberaChatHostName;
-struct sockaddr_in LiberaChat;
+/* NOTE
+ * rawIRCServers should be inserted like: irc.server0.sever0:port,irc.server1.server1:port
+ */
 
-char nickname[MAX_NICKNAME_LENGHT];
-char password[MAX_PASSWORD_LENGHT];
-
-char serverMessages[MAX_MESSAGES], clientMessages[MAX_MESSAGES];
-
-int initializeSocket();
-int initializeServers();
-int connectToIRCServer(struct sockaddr_in IRCServer);
+int resolveDNS();
 
 int main(int argc, char *argv[]) {
-    if(argc < 3) {
-        printf("\n[ ERROR / main() ]\n\targv[1] : nickname\n\targv[1] : password\n");
+    if(argc < EXPECTED_ARGC) {
+        printf("invalid arguments\n");
 
-        return -1;
+        return 1;
     } else {
         strcpy(nickname, argv[1]);
         strcpy(password, argv[2]);
+        strcpy(rawIRCServers, argv[3]);
 
-        printf("\n[ DEBUG / main() / argc, argv ]\n\tnickname : %s\n\tpassword : %s\n", nickname, password);
-
-    }
-
-    if(initializeSocket() == -1) return -1;
-
-    if(initializeServers() == -1) return -1;
-
-    if(connectToIRCServer(/* TESTING */LiberaChat) == -1) return -1;
-
-    while(true) {
-        read(clientFd, serverMessages, 255);
-        printf("\n > %s\n", serverMessages);
+        printf("debug:\n\tnickname: %s\n\tpassword: %s\n", nickname, password);
     }
 
     return 0;
 }
 
-int initializeSocket() {
-    clientFd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(clientFd < 0) {
-        printf("\n[ ERROR / initializeSocket() / socket ]\n\tFailed to create socket\n");
-        
-        return -1;
-    }
-
-    printf("\n[ DEBUG / initializeSocket() / socket ]\n\tSocket created\n");
-
-    return 0;
-}
-
-int initializeServers() {
-    // LiberaChat.sin_addr.s_addr = inet_addr("51.89.226.78"); // irc.libera.chat
-    if(inet_pton(AF_INET, "51.89.226.78", &LiberaChat.sin_addr) <= 0) {
-        printf("\n[ ERROR / initializeServers() / inet_pton ]\n\tInvalid address or address not supported\n");
-
-        return -1;
-    }
-    LiberaChat.sin_family = AF_INET;
-    LiberaChat.sin_port = htons(6697);
-
-    return 0;
-}
-
-int connectToIRCServer(struct sockaddr_in IRCServer) {
-    printf("\n[ DEBUG / connectToIRCServer() ]\n\tAttempting to connect to:\n\tsin_addr : %s\n\tsin_port : %d\n", inet_ntoa(IRCServer.sin_addr), IRCServer.sin_port);
-    
-    if(connect(clientFd, (struct sockaddr *)&IRCServer, sizeof(IRCServer)) < 0) {
-        printf("\n[ ERROR / connectToIRCServer() ]\n\tUnable to connect\n");
-        
-        return -1;
-    }
-
-    printf("\n[ DEBUG / connectToIRCServer() ]\n\tConnected\n");
-
-    return 0;
+int resolveDNS() {
 }
