fbaf144bd6c0b5548f62c51613b214519f729e31
Samuele <samuele.cistaro@gmail.com>
2024-03-26 11:04:39 +0100

    the login seems working PONG needs to be implemented commands for bots different from NickServ need to be implemented working on a way to keep the connection open after login

--- netcat-as-irc-client/netcat-as-irc-client.sh
+++ netcat-as-irc-client/netcat-as-irc-client.sh
@@ -3,39 +3,14 @@
 # $3 Nickname
 # $4 Password
 
-BOT_NAME="NickServ" # Most common / Default one
-
-if [ $1 == "*quake*" ]; then
-    BOT_NAME="Q"
+if [ ${1} == '*quacke*' ]; then
+    SERVER_BOT="Q"
+else
+    SERVER_BOT="NickServ"
 fi
 
-main() {
-    starting
+echo "NICK ${3}" >> loginCommands.txt
+echo "USER ${3} * * :${3}" >> loginCommands.txt
+echo "privmsg ${SERVER_BOT} :IDENTIFY ${3} ${4}" >> loginCommands.txt
 
-    connect
-
-    login
-
-    pong
-}
-
-starting() {
-    echo
-    echo "Connecting to: ${1}:${2}"
-    echo "Nickname: ${3}"
-    echo "Password: ${4}"
-    echo
-}
-
-connect() {
-}
-
-login() {
-    echo "NICK ${3}"
-    echo "USER ${3} * * :${3}"
-    echo "privmsg ${BOT_NAME} :IDENTIFY ${3} ${4}"
-}
-
-pong() {
-    
-}
+nc ${1} ${2} < loginCommands.txt
